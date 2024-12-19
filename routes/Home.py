dc605c274074f978ebb63cab44f154f738e47df9
K23029KK <k23029kk@aitech.ac.jp>
2024-12-19 19:20:48 +0900

    ブループリントの作成、パスの定義方法の変更
    ブループリントの作成とパスの定義を課題と同じ形式に変更しました。いくつかコメントもつけておいたので、参考までに。

--- routes/Home.py
+++ routes/Home.py
@@ -1,1 +1,8 @@
-print("routes")
+from flask import Blueprint, render_template
+
+# Blueprintを作成
+home_bp = Blueprint('home', __name__, url_prefix='/home')
+
+@home_bp.route('/')
+def day_data():
+    return render_template('Day_data.html')
