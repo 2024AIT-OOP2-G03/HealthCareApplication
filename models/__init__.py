ac7501dc73e698e02fffa9422a84028ff26c0506
hamaguchi21 <k23100kk@aitech.ac.jp>
2025-01-16 09:55:02 +0900

    修正

--- models/__init__.py
+++ models/__init__.py
@@ -15,13 +15,6 @@ MODELS = [
 ]
 
 def initialize_database():
-    print("Initializing database...")
-    if not db.is_closed():  # 接続が開いている場合
-        db.close()  # 接続を閉じる
-        print("既存のデータベース接続を閉じました。")
-    try:
-        db.connect()  # 新たに接続を開く
-        db.create_tables(MODELS, safe=True)
-        print("データベースの初期化が完了しました。")
-    except Exception as e:
-        print(f"データベース初期化中にエラーが発生しました: {e}")
+    db.connect()  # 新たに接続を開く
+    db.create_tables(MODELS, safe=True)
+    db.close()  # 接続を閉じる
